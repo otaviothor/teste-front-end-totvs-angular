59a34fdfe54e27d2adcd6d732c44be31e081b085
Otávio Silva <otaviosilva2632@gmail.com>
2025-12-04 23:45:44 -0300

    feat: finish components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Select } from './components/select/select';
+import { Switch } from './components/switch/switch';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Switch, Select],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
